1bf0185e837603b160695284cc1c05f77df716ad
anshu-govtech <anshu@dsaid.gov.sg>
2022-12-29 17:15:57 +0800

    Added aws version of pipelinedp on spark

--- run_pipelinedp_spark_aws.py
+++ run_pipelinedp_spark_aws.py
@@ -1,9 +1,11 @@
 """Using Google's and OpenMinded's PipelineDP library to execute differentially private queries"""
 
 import os
+import sys
 import time
 import psutil
 import pandas as pd
+import numpy as np
 from tqdm import tqdm
 
 import pyspark
@@ -11,16 +13,137 @@ from pipeline_dp.private_spark import make_private
 import pipeline_dp
 from pipeline_dp.aggregate_params import VarianceParams, MeanParams, SumParams, CountParams
 
-from commons.stats_vals import PIPELINEDP
-from commons.stats_vals import BASE_PATH, EPSILON_VALUES, MEAN, VARIANCE, COUNT, SUM
-from commons.utils import save_synthetic_data_query_ouput, update_epsilon_values
+# from commons.stats_vals import PIPELINEDP
+# from commons.stats_vals import BASE_PATH, EPSILON_VALUES, MEAN, VARIANCE, COUNT, SUM
+# from commons.utils import save_synthetic_data_query_ouput, update_epsilon_values
 
 #-----------#
 # Constants #
 #-----------#
-LIB_NAME = PIPELINEDP
+LIB_NAME = "PIPELINEDP"
 
-# Framework independent function
+
+#-------- Change for AWS -----------------------#
+import boto3
+import botocore
+from awsglue.utils import getResolvedOptions
+
+LIB_NAME = "PIPELINEDP"
+
+# queries to experiment
+MEAN = "MEAN"
+COUNT = "COUNT"
+SUM = "SUM"
+VARIANCE = "VARIANCE"
+
+EPSILON_VALUES = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
+                  0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+                  1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+S3_DATA_BUCKET = "dp-experiments-data-public"
+S3_OUTPUT_BUCKET = "dp-experiments-outputs"
+
+
+def update_epsilon_values(output_file):
+    """
+    """
+    out_df = pd.read_csv(output_file)
+    last_epsilon = out_df["epsilon"].iloc[-1]
+
+    index = EPSILON_VALUES.index(last_epsilon)
+
+    try:
+        return EPSILON_VALUES[index+1:]
+    except:
+        return -1
+
+
+def strip_end(text, suffix):
+    """
+    """
+    if suffix and text.endswith(suffix):
+        return text[:-len(suffix)]
+    return text
+
+
+def save_synthetic_data_query_ouput(lib_name, query, epsilon, filename, error, relative_errors, scaled_errors, time_used, memory_used):
+    """
+    """
+
+    rounding_val = 2
+    out = {}
+
+    out["epsilon"] = epsilon
+
+    # data_<size>_<scale>_<skew>.csv
+    data_feats = filename.split("_")
+    out["dataset_size"] = data_feats[1]
+    out["dataset_scale"] = data_feats[2]
+    out["dataset_skew"] = strip_end(data_feats[3], ".csv")
+
+    out["mean_error"] = round(np.mean(error), rounding_val)
+    out["stdev_error"] = round(np.std(error), rounding_val)
+
+    out["mean_relative_error"] = round(np.mean(relative_errors), rounding_val)
+    out["stdev_relative_error"] = round(np.std(relative_errors), rounding_val)
+
+    out["mean_scaled_error"] = round(np.mean(scaled_errors), rounding_val)
+    out["stdev_scaled_error"] = round(np.std(scaled_errors), rounding_val)
+
+    out["mean_time_used"] = round(np.mean(time_used), rounding_val)
+    out["mean_memory_used"] = round(np.mean(memory_used), rounding_val)
+
+    df = pd.DataFrame([out])
+
+    directory = f"outputs/synthetic/{lib_name.lower()}/size_{out['dataset_size']}/"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    output_path = directory + f"{query.lower()}.csv"
+    df.to_csv(output_path, mode="a", header=not os.path.exists(
+        output_path), index=False)
+
+    print(f"Saved results for epsilon: {epsilon}")
+
+
+def save_synthetic_data_query_output_aws(s3_path, lib_name, query, epsilon, filename, error, relative_errors, scaled_errors, time_used, memory_used):
+    """
+    """
+
+    rounding_val = 2
+    out = {}
+
+    out["epsilon"] = epsilon
+
+    # data_<size>_<scale>_<skew>.csv
+    data_feats = filename.split("_")
+    out["dataset_size"] = data_feats[1]
+    out["dataset_scale"] = data_feats[2]
+    out["dataset_skew"] = strip_end(data_feats[3], ".csv")
+
+    out["mean_error"] = round(np.mean(error), rounding_val)
+    out["stdev_error"] = round(np.std(error), rounding_val)
+
+    out["mean_relative_error"] = round(np.mean(relative_errors), rounding_val)
+    out["stdev_relative_error"] = round(np.std(relative_errors), rounding_val)
+
+    out["mean_scaled_error"] = round(np.mean(scaled_errors), rounding_val)
+    out["stdev_scaled_error"] = round(np.std(scaled_errors), rounding_val)
+
+    out["mean_time_used"] = round(np.mean(time_used), rounding_val)
+    out["mean_memory_used"] = round(np.mean(memory_used), rounding_val)
+
+    df = pd.DataFrame([out])
+
+    directory = f"s3://{s3_path}/outputs/synthetic/{lib_name.lower()}/size_{out['dataset_size']}/"
+
+    output_path = directory + f"{query.lower()}.csv"
+    df.to_csv(output_path, mode="a", header=not os.path.exists(
+        output_path), index=False)
+
+    print(f"Saved results for epsilon: {epsilon}")
+
+#-----------------------------------------------#
 
 
 def get_spark_context():
@@ -115,139 +238,176 @@ def run_pipelinedp_query(query, epsilon_values, per_epsilon_iterations, data_pat
     #------------#
     # Datasets   #
     #------------#
-    for filename in os.listdir(data_path):
+    client = boto3.client('s3')
+    paginator = client.get_paginator('list_objects_v2')
+    result = paginator.paginate(Bucket=S3_DATA_BUCKET)
+    for page in result:
+        if "Contents" in page:
+            for key in page["Contents"]:
+                filename = key["Key"]
 
-        print("#"*10)
-        print("Filename: ", filename)
-        print("#"*10)
-        if not filename.endswith(".csv"):
-            continue
+                # looping through the S3 on the appropriate data size subfolder
+                if filename.startswith(f"synthetic_data/size_{dataset_size}/"):
+                    for filename in os.listdir(data_path):
 
-        df = pd.read_csv(data_path + filename, skiprows=0)
-        data = df[column_name]
-        num_rows = data.count()
+                        print("#"*10)
+                        print("Filename: ", filename)
+                        print("#"*10)
+                        if not filename.endswith(".csv"):
+                            continue
 
-        try:
-            # load file data into Spark's RDD
-            data_rdd = saprk_context.textFile(
-                data_path + filename).mapPartitions(_parse_partition)
-        except Exception as e:
-            print(e)
+                        # df = pd.read_csv(data_path + filename)
+                        df = pd.read_csv(f"s3a://{S3_DATA_BUCKET}/{filename}")
+                        data = df[column_name]
+                        num_rows = data.count()
 
-        # breakpoint()
+                        try:
+                            # load file data into Spark's RDD
+                            data_rdd = saprk_context.textFile(
+                                f"s3a://{S3_DATA_BUCKET}/{filename}").mapPartitions(_parse_partition)
+                        except Exception as e:
+                            print(e)
 
-        #----------#
-        # Epsilons #
-        #----------#
-        for epsilon in epsilon_values:
+                        # breakpoint()
 
-            print("epsilon: ", epsilon)
+                        #----------#
+                        # Epsilons #
+                        #----------#
+                        for epsilon in epsilon_values:
 
-            eps_time_used = []
-            eps_memory_used = []
-            eps_errors = []
-            eps_relative_errors = []
-            eps_scaled_errors = []
+                            print("epsilon: ", epsilon)
 
-            #------------------------#
-            # Per epsilon iterations #
-            #------------------------#
-            for _ in tqdm(range(per_epsilon_iterations)):
+                            eps_time_used = []
+                            eps_memory_used = []
+                            eps_errors = []
+                            eps_relative_errors = []
+                            eps_scaled_errors = []
 
-                begin_time = time.time()
+                            #------------------------#
+                            # Per epsilon iterations #
+                            #------------------------#
+                            for _ in tqdm(range(per_epsilon_iterations)):
 
-                budget_accountant = pipeline_dp.NaiveBudgetAccountant(
-                    total_epsilon=100, total_delta=1e-7)
+                                begin_time = time.time()
 
-                # wrap Spark's RDD into its private version
-                private_rdd = make_private(
-                    data_rdd, budget_accountant, lambda val: val["id"])
+                                budget_accountant = pipeline_dp.NaiveBudgetAccountant(
+                                    total_epsilon=100, total_delta=1e-7)
 
-                process = psutil.Process(os.getpid())
+                                # wrap Spark's RDD into its private version
+                                private_rdd = make_private(
+                                    data_rdd, budget_accountant, lambda val: val["id"])
 
-                #----------------------------------------#
-                # Compute differentially private queries #
-                #----------------------------------------#
-                if query == COUNT:
-                    dp_result = _compute_dp_count(private_rdd, epsilon)
-                else:
-                    min_value = data.min()
-                    max_value = data.max()
+                                process = psutil.Process(os.getpid())
 
-                    if query == MEAN:
-                        dp_result = _compute_dp_mean(
-                            private_rdd, epsilon, min_value, max_value)
-                    elif query == SUM:
-                        dp_result = _compute_dp_sum(
-                            private_rdd, epsilon, min_value, max_value)
-                    elif query == VARIANCE:
-                        dp_result = _compute_dp_variance(
-                            private_rdd, epsilon, min_value, max_value)
+                                #----------------------------------------#
+                                # Compute differentially private queries #
+                                #----------------------------------------#
+                                if query == COUNT:
+                                    dp_result = _compute_dp_count(private_rdd, epsilon)
+                                else:
+                                    min_value = data.min()
+                                    max_value = data.max()
 
-                budget_accountant.compute_budgets()
-                
-                # rdd action
-                private_value = dp_result.collect()[0][1]
+                                    if query == MEAN:
+                                        dp_result = _compute_dp_mean(
+                                            private_rdd, epsilon, min_value, max_value)
+                                    elif query == SUM:
+                                        dp_result = _compute_dp_sum(
+                                            private_rdd, epsilon, min_value, max_value)
+                                    elif query == VARIANCE:
+                                        dp_result = _compute_dp_variance(
+                                            private_rdd, epsilon, min_value, max_value)
 
-                # compute execution time
-                eps_time_used.append(time.time() - begin_time)
+                                budget_accountant.compute_budgets()
+                                
+                                # rdd action
+                                private_value = dp_result.collect()[0][1]
 
-                # compute memory usage
-                eps_memory_used.append(process.memory_info().rss)
+                                # compute execution time
+                                eps_time_used.append(time.time() - begin_time)
 
-                #---------------------#
-                # Compute true values #
-                #---------------------#
-                if query == MEAN:
-                    true_value = data.mean()
-                elif query == SUM:
-                    true_value = data.sum()
-                elif query == VARIANCE:
-                    true_value = data.var()
-                elif query == COUNT:
-                    true_value = num_rows
+                                # compute memory usage
+                                eps_memory_used.append(process.memory_info().rss)
 
-                # print("min_value: ", min_value)
-                # print("max_value: ", max_value)
-                print("true_value:", true_value)
-                print("private_value:", private_value)
+                                #---------------------#
+                                # Compute true values #
+                                #---------------------#
+                                if query == MEAN:
+                                    true_value = data.mean()
+                                elif query == SUM:
+                                    true_value = data.sum()
+                                elif query == VARIANCE:
+                                    true_value = data.var()
+                                elif query == COUNT:
+                                    true_value = num_rows
 
-                # compute errors
-                error = abs(true_value - private_value)
+                                # print("min_value: ", min_value)
+                                # print("max_value: ", max_value)
+                                print("true_value:", true_value)
+                                print("private_value:", private_value)
 
-                eps_errors.append(error)
-                eps_relative_errors.append(error/abs(true_value))
-                eps_scaled_errors.append(error/num_rows)
+                                # compute errors
+                                error = abs(true_value - private_value)
 
-            save_synthetic_data_query_ouput(LIB_NAME, query, epsilon, filename, eps_errors,
-                                            eps_relative_errors, eps_scaled_errors, eps_time_used, eps_memory_used)
+                                eps_errors.append(error)
+                                eps_relative_errors.append(error/abs(true_value))
+                                eps_scaled_errors.append(error/num_rows)
 
+                            save_synthetic_data_query_ouput(LIB_NAME, query, epsilon, filename, eps_errors,
+                                                            eps_relative_errors, eps_scaled_errors, eps_time_used, eps_memory_used)
 
 if __name__ == "__main__":
 
     #----------------#
     # Configurations #
     #----------------#
-    experimental_query = MEAN  # {MEAN, VARIANCE, COUNT, SUM}
 
-    dataset_size = 10  # {}
+    s3 = boto3.resource('s3')
+
+    args = getResolvedOptions(sys.argv,
+                              ['JOB_NAME',
+                               'pipelinedp_query',
+                               'pipelinedp_iterations',
+                               'pipelinedp_dataset_size'])
+
+    # {MEAN, VARIANCE, COUNT, SUM}
+    experimental_query = args['pipelinedp_query']
+
+    dataset_size = int(args['pipelinedp_dataset_size'])
 
     # path to the folder containing CSVs of `dataset_size` size
-    dataset_path = BASE_PATH + f"datasets/synthetic_data/size_{dataset_size}/"
+    dataset_path = f"s3://{S3_DATA_BUCKET}/synthetic_data/size_{dataset_size}/"
+    # dataset_path = BASE_PATH + f"datasets/synthetic_data/size_{dataset_size}/"
 
     # for synthetic datasets the column name is fixed (will change for real-life datasets)
     column_name = "values"
 
     # number of iterations to run for each epsilon value
     # value should be in [100, 500]
-    per_epsilon_iterations = 3  # for the testing purpose low value is set
+    # for the testing purpose low value is set
+    per_epsilon_iterations = int(args['pipelinedp_iterations'])
 
     epsilon_values = EPSILON_VALUES
 
     # get the epsilon values to resume with
-    output_file = f"outputs/synthetic/{LIB_NAME.lower()}/size_{dataset_size}/{experimental_query}.csv"
-    if os.path.exists(output_file):
-        epsilon_values = update_epsilon_values(output_file)
+    output_file = f"outputs/synthetic/{LIB_NAME.lower()}/size_{dataset_size}/{experimental_query.lower()}.csv"
+
+    # check if file already exists in S3
+    try:
+        s3.Object(S3_DATA_BUCKET, output_file).load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print(f"The file {output_file} does not exist in {S3_DATA_BUCKET}")
+        else:
+            raise
+    else:
+        epsilon_values = update_epsilon_values(
+            f"s3://{S3_DATA_BUCKET}/{output_file}")
+        print(
+            f"Epsilon value check before conducting experiment: {epsilon_values}")
+
+    # if os.path.exists(output_file):
+    #     epsilon_values = update_epsilon_values(output_file)
 
     # test if all the epsilon values have NOT been experimented with
     if epsilon_values != -1:
@@ -261,3 +421,5 @@ if __name__ == "__main__":
 
         run_pipelinedp_query(experimental_query, epsilon_values,
                              per_epsilon_iterations, dataset_path, column_name)
+    else:
+        print("Experiment for these params were conducted before.")
